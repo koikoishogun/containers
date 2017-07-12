60a285ed43a12bb44c4cda3cb18e3f4e653bc595
MianoMaina <mianodmaina@gmail.com>
2017-07-13 01:42:33 +0300

    homepage commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,8 @@
 		<script  src="/js/welcome.js"></script>
 
         <link href="{{ URL::asset('/css/bootstrap.min.css')}}" rel="stylesheet">
-
-        <title>Laravel</title>
+        <link href="{{ URL::asset('/css/containerskenya.css')}}" rel="stylesheet">
+        <title>Containers Kenya</title>
 
        
 
@@ -19,18 +19,116 @@
 	
     <body>
 
-    <p>
-    Check
-    </p>
+       <div id="container-fluid">
+            <div class="container">
+                <img class="logo-icon img-responsive" src="{{ URL::asset('/images/logo-icon.png')}}"/>
+                <img class="logo-name img-responsive" src="{{ URL::asset('/images/logo-name.png')}}"/>
+
+            </div>
+       </div>
+
+       <div class="slider">
+            <div id = "myCarousel" class = "carousel slide">
+       
+               <!-- Carousel indicators -->
+               <ol class = "carousel-indicators">
+                  <li data-target = "#myCarousel" data-slide-to = "0" class = "active"></li>
+                  <li data-target = "#myCarousel" data-slide-to = "1"></li>
+                  <li data-target = "#myCarousel" data-slide-to = "2"></li>
+                  <li data-target = "#myCarousel" data-slide-to = "3"></li>
+               </ol>   
+               
+               <!-- Carousel items -->
+               <div class = "carousel-inner">
+                  
+                  <div class = "item active">
+                        <div class="container">
+                          <div class="row">
+                              <div class="col-md-6">
+                                     <img class="slider-img img-responsive" src = "{{ URL::asset('/images/cafe.png')}}" alt = "First slide">
+                              </div>
+
+                              <div class="col-md-5">
+                                <h1 class="white big-header"><b>Fabricated Cafe</b></h1>
+                                <h3 class="white running-line">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</h3>
+
+                                <p class="white">orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidat</p>
+
+                                <div id="empty">
+                                     <button class="order-btn" type="button" id="sub_btn"><b>Order Now</b></button>
+                                     <button class="catalogue-btn" type="button" id="sub_btn"><b>Just Send Me a Catalogue</b></button>
+                                </div>
+
+                             
+                              </div>
+                          </div>
+                        </div>
+                  </div>
+                  
+                  <div class = "item">
+                    <div class="container">
+                         <div class="row">
+                              <div class="col-md-6">
+                                     <img class="slider-img img-responsive" src = "{{ URL::asset('/images/house.png')}}" alt = "First slide">
+                              </div>
+
+                              <div class="col-md-6">
+                                <h1 class="white big-header"><b>Container Homes</b></h1>
+                                <h3 class="white running-line">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</h3>
+
+                                <p class="white">orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidat</p>
+
+                                 <div id="empty">
+                                     <button class="order-btn" type="button" id="sub_btn"><b>Order Now</b></button>
+                                     <button class="catalogue-btn" type="button" id="sub_btn"><b>Just Send Me a Catalogue</b></button>
+                                </div>
+
+                              </div>
+
+                          </div>
+                    </div>
+                </div>
+                  
+                  <div class = "item">
+                      <div class="container">
+                         <div class="row">
+                              <div class="col-md-6">
+                                     <img class="slider-img img-responsive" src = "{{ URL::asset('/images/stall.png')}}" alt = "First slide">
+                              </div>
+
+                              <div class="col-md-6">
+                                <h1 class="white big-header"><b>Stalls & Shops</b></h1>
+                                <h3 class="white running-line">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</h3>
+
+                                    <p class="white">orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidat</p>
+
+                                     <div id="empty">
+                                         <button class="order-btn" type="button" id="sub_btn"><b>Order Now</b></button>
+                                         <button class="catalogue-btn" type="button" id="sub_btn"><b>Just Send Me a Catalogue</b></button>
+                                    </div>
+
+                              </div>
+                          </div>
+                       </div>
+                  </div>
+
+               </div>
+               
+               <!-- Carousel nav -->
+               <a class = "carousel-control left" href = "#myCarousel" data-slide = "prev">&lsaquo;</a>
+               <a class = "carousel-control right" href = "#myCarousel" data-slide = "next">&rsaquo;</a>
+       
+            </div> 
+       </div>
+
+
+      <div id="empty">
+         <button type="button" id="sub_btn">Subscribe</button>
+      </div>
+
         <script src="{{ URL::asset('/js/jquery.js')}}"></script>
         <script src="{{ URL::asset('/js/bootstrap.js')}}"></script>
         <script src="{{ URL::asset('/js/omega.js')}}"></script>
-	  <div id="empty">
-	     <button type="button" id="sub_btn">Subscribe</button>
-	  
-	  </div>
-	
-        
 
     </body>
    </html>
