7da229de82de23f3ad3a43eba5c8217009b987f0
Sayeed Sydney <sydneysayeed@gmail.com>
2017-08-15 22:32:31 +0300

    trying to save blob to db

--- database/migrations/2017_07_26_213138_create_posts_table.php
+++ database/migrations/2017_07_26_213138_create_posts_table.php
@@ -33,7 +33,7 @@ class CreatePostsTable extends Migration
 			$table->string('title');
 			$table->string('name');
 			$table->text('body');
-			$table->binary('image');
+			$table->longtext('image');
 			//$table->integer('featured')->null();
 			
 			//$table->foreign('image')->references('id')->on('uploads');
