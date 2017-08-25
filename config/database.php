b8514e9f6a78c1a03bc181fdf0b9b15a0b64a034
Don Xihote <sydneysayeed@gmail.com>
2017-08-25 21:01:55 +0300

    fixed db error

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
 			'host' => $host,//env('DB_HOST', '127.0.0.1'),
             //'port' => env('DB_PORT', '3306'),
             'database' => $database,//env('DB_DATABASE', 'forge'),
-            'username' =>$userneme ,//env('DB_USERNAME', 'forge'),
+            'username' =>$username ,//env('DB_USERNAME', 'forge'),
             'password' =>$password,//env('DB_PASSWORD', ''),
             //'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
