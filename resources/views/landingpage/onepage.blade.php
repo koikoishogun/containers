ea7d8e218d08a300b9912d04dda3d0d071c6ff0e
dennismiano <18631680+dennismiano@users.noreply.github.com>
2017-09-29 11:11:08 +0300

    Update onepage.blade.php

--- resources/views/landingpage/onepage.blade.php
+++ resources/views/landingpage/onepage.blade.php
@@ -15,14 +15,16 @@
         <link href="{{ URL::asset('/css/containerskenya.css')}}" rel="stylesheet">
         <title>Containers Kenya</title>
 
-          <!-- Global Site Tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107155686-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)};
-              gtag('js', new Date());
-              gtag('config', 'UA-107155686-1');
-            </script>
+         <!-- Global Site Tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107155686-2"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments)};
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-107155686-2');
+	</script>
+
 
     </head>
   
